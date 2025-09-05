7780308d5b172ebf2a15989a41797269a5f1a12d
akshay kumar <124570439+akshay-node@users.noreply.github.com>
2025-09-05 09:09:52 +0530

    Update db.js

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -2,11 +2,11 @@ import mysql from "mysql2/promise";
 
 export async function connectDB() {
   const connection = await mysql.createConnection({
-    host: "localhost",
-    port: 3307, 
-    user: "root",
-    password: "",
-    database: "schooldb"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   });
   return connection;
 }
