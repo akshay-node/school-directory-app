6f89ff9e11fd58f47f63a9603fea385730c77dbb
akshay kumar <124570439+akshay-node@users.noreply.github.com>
2025-09-05 16:31:16 +0530

    Update db.js

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -1,13 +1,15 @@
 import mysql from "mysql2/promise";
 
-const url = new URL(process.env.MYSQL_PUBLIC_URL);
+export async function connectDB() {
+  const url = new URL(process.env.MYSQL_PUBLIC_URL);
 
-const connection = await mysql.createConnection({
-  host: url.hostname,
-  port: url.port,
-  user: url.username,
-  password: url.password,
-  database: url.pathname.replace("/", ""),
-});
+  const connection = await mysql.createConnection({
+    host: url.hostname,
+    port: url.port,
+    user: url.username,
+    password: url.password,
+    database: url.pathname.replace("/", ""),
+  });
+
   return connection;
 }
